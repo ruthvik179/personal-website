fc7d404cea216e8c10f6d0734f046273be74866b
ruthvik179 <ruthvikkadiyala.179@gmail.com>
2020-05-31 20:17:45 +0530

    initial

---
+++ src/components/sections/Skills.js
@@ -0,0 +1,67 @@
+import React from "react";
+import SkillsList from "../elements/SkillsList";
+import Resume from "../../resume.json";
+
+function Skills() {
+  return (
+    <section className="section" id="skills">
+      <div className="container">
+        <h1 className="title">Skills</h1>
+        <div className="columns">
+          <div className="column is-6">
+            <div className="has-text-centered">
+              <span className="icon has-text-danger">
+                <i className="fas fa-3x fa-laptop-code"></i>
+              </span>
+              <h2 className="title is-5">Software Development</h2>
+            </div>
+            <SkillsList
+              skills={Resume.skills
+                .filter(skill => skill.keywords.includes("Software Developer"))
+                .reduce((obj, item) => {
+                  obj[item.name] = item.level;
+                  return obj;
+                }, {})}
+            />
+          </div>
+          <div className="column is-6">
+            <div className="row is-6">
+              <div className="has-text-centered">
+                <span className="icon has-text-danger">
+                  <i className="fas fa-3x fa-laptop-code"></i>
+                </span>
+                <h2 className="title is-5">Languages</h2>
+              </div>
+              <SkillsList
+                skills={Resume.skills
+                  .filter(skill => skill.keywords.includes("Programming Languages"))
+                  .reduce((obj, item) => {
+                    obj[item.name] = item.level;
+                    return obj;
+                  }, {})}
+              />
+            </div>
+            <div className="row is-6" style={{margin: "4% 0 0 0"}}>
+              <div className="has-text-centered">
+                <span className="icon has-text-danger">
+                  <i className="fas fa-3x fa-laptop-code"></i>
+                </span>
+                <h2 className="title is-5">Frameworks</h2>
+              </div>
+              <SkillsList
+                skills={Resume.skills
+                  .filter(skill => skill.keywords.includes("Frameworks"))
+                  .reduce((obj, item) => {
+                    obj[item.name] = item.level;
+                    return obj;
+                  }, {})}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
---
+++ src/components/structure/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer has-background-danger">
+      <div className="content has-text-centered has-text-white">
+        <p>
+          Built at <i className="fas fa-moon"></i> by{" "}
+          <a href="https://github.com/ruthvik179" className="has-text-white">
+            <strong>Ruthvik Kadiyala</strong>
+          </a>
+        </p>
+        <p>
+          <img
+            width="512"
+            height="96"
+            src="https://bulma.io/images/made-with-bulma--semiwhite.png"
+            className="bulma-image"
+            alt=""
+          />
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
