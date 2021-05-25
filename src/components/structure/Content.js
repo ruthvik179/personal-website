e8b48616cc12912a539d91c8fe45a458df899c36
ruthvik179 <64011689+ruthvik179@users.noreply.github.com>
2021-05-25 20:04:54 +0530

    Update Content.js

--- src/components/structure/Content.js
+++ src/components/structure/Content.js
@@ -2,7 +2,6 @@ import React from "react";
 import AboutMe from "../sections/AboutMe";
 import Skills from "../sections/Skills";
 import Experience from "../sections/Experience";
-import Articles from "../sections/Articles";
 
 function Content() {
   return (
@@ -10,7 +9,6 @@ function Content() {
       <AboutMe />
       <Skills />
       <Experience />
-      <Articles />
     </main>
   );
 }
